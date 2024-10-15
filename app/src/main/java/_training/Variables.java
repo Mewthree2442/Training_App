1901666891161085a639ace47f3f8c6c68b0903e
NickTheFox99 <breakoutboy99@gmail.com>
2024-10-15 17:32:02 -0500

    Write the Variables Section
     - Added the variables section, which includes declaration, usage, and types
     - Added Variables.java for a template to follow along
     - Added VariablesSolution.java for the "solution"

---
+++ app/src/main/java/_training/Variables.java
@@ -0,0 +1,12 @@
+package _training;
+
+public class Variables {
+    public static void main(String[] args) {
+        int myNum = 1;
+        int myOtherNum = 2;
+
+        // Get the sum of the two numbers
+        int myFinalNum = myNum + myOtherNum;
+        System.out.println(myFinalNum);
+    }
+}
