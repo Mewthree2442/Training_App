72ebd0772302281917c1a58b3a537e566d52a8d7
NickTheFox99 <breakoutboy99@gmail.com>
2024-10-29 18:50:03 -0500

    Write Return Values
    - Revert overloading lead
     - Overloading may be too complicated for beginners
     - May write overloading later if really needed
    - Lead into return values
     - Use current issue with functions before return values
    - Write Return Values in readme
     - Teaches return types
     - Teaches return uses
    - Plan (setup todo) for using "machines" explanation
      - Teaching functions like machines

---
+++ app/src/main/java/_training/ReturnSolution.java
@@ -0,0 +1,11 @@
+package _training;
+
+public class ReturnSolution {
+    static float square(float num){
+        return num*num;
+    }
+    public static void main(String[] args){
+        System.out.println(square(5) + square(2));
+        // int output = square(3); // Does not work!
+    }
+}
