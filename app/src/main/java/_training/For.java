070a9f0011aaf1a3b8a1f0265a3bbfef2eeeae25
NickTheFox99 <breakoutboy99@gmail.com>
2024-11-05 20:52:13 -0600

    Start work on For loops  - Write beginning of README section  - Start explaining iterations and purpose

---
+++ app/src/main/java/_training/For.java
@@ -0,0 +1,9 @@
+package _training;
+
+public class For {
+    public static void main(String[] args) {
+        for (int i = 0; i < 20; i++) {
+            System.out.println("I ran in " + i + " circles today!");
+        }
+    }
+}
---
+++ app/src/main/java/_training/ForSolution.java
@@ -0,0 +1,9 @@
+package _training;
+
+public class ForSolution {
+    public static void main(String[] args){
+        for (int i = 0; i < 30; i++){
+            System.out.println("I ran in "+i+" circles today!");
+        }
+    }
+}
