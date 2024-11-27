9c3a5f236b5952bd809a8a240328004a3537627c
theomatter2@gmail.com <theomatter2@gmail.com>
2024-11-26 21:07:18 -0600

    Changes at 930 Meeting

--- app/src/main/java/_training/IfStatement.java
+++ app/src/main/java/_training/IfStatement.java
@@ -9,6 +9,19 @@ public class IfStatement {
 
         if((brushedTeeth || tookMints) && didHomework && preparedForRobotics){
             System.out.println("I can go to robotics tonight!");
+        } else{
+            if(!(brushedTeeth || tookMints)){
+                System.out.println("Gotta freshen up!");
+            }
+            if(!didHomework){
+                System.out.println("Oh no! I need to study for Kim's test and finish the project!");
+            }
+            if(!preparedForRobotics) {
+                System.out.println("I need to finish finding sponsors");
+            }
+
+            System.out.println("I can't go to robotics yet. :(");
+
         }
     }
 }
--- app/src/main/java/_training/Variables.java
+++ app/src/main/java/_training/Variables.java
@@ -4,9 +4,25 @@ public class Variables {
     public static void main(String[] args) {
         int myNum = 1;
         int myOtherNum = 2;
+        int myNumThree = 7;
 
         // Get the sum of the two numbers
         int myFinalNum = myNum + myOtherNum;
+        int myNumFour = myFinalNum * myNumThree;
         System.out.println(myFinalNum);
+        System.out.println(myNumFour);
+        String myText = "Hello";
+        String myOtherText = "again!";
+        String myFinalText = myText + " " + myOtherText;
+        System.out.println(myFinalText);
+        float myDecimal = 2.5f;
+        float mySecondDecimal = 5.0f;
+        float myLastDecimal = myDecimal / mySecondDecimal;
+        System.out.println(myLastDecimal);
+        boolean ateBreakfast = true;
+        boolean ateLunch = true; 
+        boolean ateDinner = false; 
+        System.out.println(ateBreakfast && ateLunch);
+        System.out.println(ateLunch && ateDinner);
     }
 }
